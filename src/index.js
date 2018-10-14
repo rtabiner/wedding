4d5e2562eeb8c3f57ee694f2b44d9f6100c5e7a8
Tabiner <rtabiner@uk-c02sp7c0g8wn.home>
2018-10-14 16:49:55 +0100

    Switches to the hashrouter.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { theme } from './Style/Theme';
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </MuiThemeProvider>,
   document.getElementById('root'),
 );
